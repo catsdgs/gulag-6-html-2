87881173af845046c7ca6d5582cb7b81963a5c54
D.J. Saderholm <catsdgs@disr.it>
2020-09-26 13:17:12 -0600

    yes

---
+++ js/submit.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+var query = getQueryParams(document.location.search);
+alert(query.query);
+});
+
+function getQueryParams(qs) {
+    qs = qs.split('+').join(' ');
+
+    var params = {},
+        tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+
+    while (tokens = re.exec(qs)) {
+        params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+    }
+
+    return params;
+}
