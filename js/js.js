73f92fd918555ab50892b2c0b6f499c7d44726b6
D.J. Saderholm <catsdgs@disr.it>
2020-12-22 17:31:58 -0700

    fsdf

--- js/js.js
+++ js/js.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
     document.getElementById("url").focus();
   }, 200);
   setTimeout(() => {
-     M.toast({html: "<p><b>Very Important Notice: Goolag domain changing soon.</b> Gulag.ga will be going down on <b>1/24/21</b> and will be replaced by <i><a href='https://goolag.app'>goolag.ga</a></i></b>.</p>"})
+     M.toast({html: "<p><b>Very Important Notice: Goolag domain changing soon.</b> Gulag.ga will be going down on <b>1/24/21</b> and will be replaced by <i><a href='https://goolag.app'>goolag.app</a></i></b>.</p>"})
   }, 2000);
 });
 
