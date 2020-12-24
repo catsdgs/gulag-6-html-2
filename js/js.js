ded21feaf44f191fe02f9b6415ef584c315573a0
D.J. Saderholm <catsdgs@disr.it>
2020-12-24 16:12:51 -0700

    hjkhjk

--- js/js.js
+++ js/js.js
@@ -1,4 +1,4 @@
-var wait = Math.floor(Math.random() * 8000) + 0;
+var wait = Math.floor(Math.random() * 12000) + 0;
 $(document).ready(function(){
   $('.tooltipped').tooltip();
   $('.modal').modal();
