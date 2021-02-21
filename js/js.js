9f9dd8b8dc4d506e63365768cafd1eb035c19089
D.J. Saderholm <catsdgs@disr.it>
2021-02-20 17:57:50 -0700

    dsfdsf

--- js/js.js
+++ js/js.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
     document.getElementById("url").focus();
   }, 150);
   setTimeout(() => {
-     M.toast({html: "<h6>Welcome to the new website! All subdomains have also changed, so if you had any bookmarks for specific websites those will no longer work and will have to be changed. We also have a new chatting service that you can access by clicking the 'Babble' link in the top right corner. Click <a class='modal-trigger' href='#newContentModal'><b>here</b></a> to view all new changes. If you do not like these changes or have feedback, you can use the <a class='modal-trigger' href='#newContentModal'>Feedback Form</a></h6>"})
+     M.toast({html: "<h6>Version 6.2 is here! Click <a class='modal-trigger' href='#newContentModal'><b>here</b></a> to view all new changes. If you do not like these changes or have feedback, you can use the <a class='modal-trigger' href='#newContentModal'>Feedback Form</a></h6>"})
   }, 1000);
 });
 
